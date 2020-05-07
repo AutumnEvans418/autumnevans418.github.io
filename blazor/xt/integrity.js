322384698e267d3ff9e95871a72fde96f5821537
Chris Evans <chrisevans9629@gmail.com>
2020-05-06 20:10:44 -0500

    updated integrity

--- blazor/xt/integrity.js
+++ blazor/xt/integrity.js
@@ -2,6 +2,6 @@
 window.onload = e => {
     let test = document.getElementsByTagName("script");
 
-    test[test.length-1].integrity = "";
+    test[test.length-1].integrity = "yVt8FYsTQDifOGsifIkmEXwe+7ML0jZ1dMi2xluiDXQ=";
 }
 
