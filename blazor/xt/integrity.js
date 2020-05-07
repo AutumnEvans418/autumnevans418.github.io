0ed205f943297b5a50298231682467c3bf75bf0b
Chris Evans <chrisevans9629@gmail.com>
2020-05-06 20:26:14 -0500

    removed integrity on all scripts for xaml template

--- blazor/xt/integrity.js
+++ blazor/xt/integrity.js
@@ -2,6 +2,9 @@
 window.onload = e => {
     let test = document.getElementsByTagName("script");
 
-    test[test.length-1].integrity = "yVt8FYsTQDifOGsifIkmEXwe+7ML0jZ1dMi2xluiDXQ=";
+    for (let index = 0; index < test.length; index++) {
+        const element = test[index];
+        element.integrity = "";
+    }
 }
 
