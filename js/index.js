3cfbb33c19403a1ba0b77a5a164b36cc5c724333
Chris Evans <chrisevans9629@gmail.com>
2020-08-08 11:20:48 -0500

    moved code

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+new Typewriter('#typewriter', {
+  strings: [
+      'Welcome to my site!',
+      'I am a software developer',
+      'Console.WriteLine("My favorite language is C#");',
+      'printfn "But I also love F#"',
+      'print("and python!")',
+      'This site was built with jekyll...',
+      'and is hosted by github pages.',
+      'Hope you enjoy and please follow me on github!'],
+  autoStart: true,
+  loop: true,
+  delay: 50,
+});
