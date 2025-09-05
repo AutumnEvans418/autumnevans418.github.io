0fc7375f8b45ae467872e69e8b44ec44f89badeb
Autumn <AutumnFox418@gmail.com>
2025-09-04 21:56:32 -0500

    updated about page

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -e
 nginx
 bundle update
 bundle install
-bundle exec jekyll serve --baseurl '' --port 8080 --watch --force_polling --livereload --incremental
+bundle exec jekyll serve --baseurl '' --port 8080 --force_polling --incremental
